Jprimero15/android_device_samsung_hlte

hlte: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
Bug: 77599627
Test: lunch  [see entries in list]
Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633
Signed-off-by: Joshua Primero <jprimero155@gmail.com>

AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/rr_hlte.mk
+
+COMMON_LUNCH_CHOICES := \
+    rr_hlte-user \
+    rr_hlte-userdebug \
+    rr_hlte-eng

Jprimero15/device_hlte_launcher

AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_hlte.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_hlte-user \
+    lineage_hlte-userdebug \
+    lineage_hlte-eng